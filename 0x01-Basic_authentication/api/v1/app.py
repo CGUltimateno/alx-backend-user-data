05e3333cc1974fa385ea5e4d2f0e2c1f10d30c21
CGUltimateno <Mohameddeaf65@gmail.com>
2024-04-16 10:24:53 +0200

    personal data stuff

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,22 +2,20 @@
 """
 Route module for the API
 """
-import os
 from api.v1.views import app_views
-from flask import jsonify, Flask, request, abort
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 auth = os.getenv('AUTH_TYPE')
-
 if auth:
     if auth == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
-
         auth = BasicAuth()
     else:
         from api.v1.auth.auth import Auth
