42aa95f2a09e95608c322cd2ef34aba0d94b9edf
CGUltimateno <Mohameddeaf65@gmail.com>
2024-04-18 11:09:48 +0200

    auth session auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -9,10 +9,10 @@ from flask import abort, jsonify, request
 from models.user import User
 from api.v1.auth.session_auth import SessionAuth
 
+
 @app_views.route('/auth_session/login',
                  methods=['POST'],
                  strict_slashes=False)
-
 def login() -> str:
     """ POST /api/v1/auth_session/login
     Return:
@@ -35,10 +35,10 @@ def login() -> str:
     response.set_cookie(os.getenv('SESSION_NAME'), session_id)
     return response
 
-@app_views.route('/auth_session/logout',
-                    methods=['DELETE'],
-                    strict_slashes=False)
 
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def logout() -> str:
     """ DELETE /api/v1/auth_session/logout
     Return:
