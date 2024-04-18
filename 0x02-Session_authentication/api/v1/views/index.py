e362713e83922f707230e41ffe9abc53f73e250b
CGUltimateno <Mohameddeaf65@gmail.com>
2024-04-18 10:47:24 +0200

    app users

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -2,32 +2,32 @@
 """
 Route module for the API
 """
-from api.v1.views import app_views
 from flask import jsonify, abort
+from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+def status() -> str:
     """Return status"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
+def stats() -> str:
     """Return stats"""
-    from models import storage
-    classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
-               "reviews": "Review", "states": "State", "users": "User"}
-    return jsonify({key: storage.count(value) for key, value in classes.items()})
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
+def unauthorized() -> None:
     """Return unauthorized"""
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden():
+def forbidden() -> None:
     """Return forbidden"""
     abort(403)
