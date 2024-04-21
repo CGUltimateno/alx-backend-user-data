fe58ead4a30fd45858e5c4f1da6fc44acf2900ee
CGU <55013791+CGUltimateno@users.noreply.github.com>
2024-04-21 02:26:10 +0200

    ses

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,7 +32,7 @@ def login() -> Tuple[str, int]:
     if not user[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
-    session_id = auth.create_session(getattr(user[0], 'id'))
+    session_id = auth.create_session(user[0].id)
     response = jsonify(user[0].to_json())
     response.set_cookie(os.getenv('SESSION_NAME'), session_id)
     return response
