65de3f4c93ec0d36ec72009a4421a5c83f67e831
CG U <Mohameddeaf65@gmail.com>
2024-04-24 03:33:07 +0000

    main testing stuff

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+"""
+Big Time Testing
+"""
+import requests
+domain: str = "http://0.0.0.0:5000/%s"
+
+
+def register_user(email: str, password: str) -> None:
+    """
+    Registeration Testing
+    """
+    url = domain % "users"
+    payload = [("email", email), ("password", password)]
+    expected = {"email": email, "message": "user created"}
+    res = requests.post(user, data=payload)
+    assert res.status_code == 200
+    assert res.json() == expected
+    expected = {"messages": "email already registered"}
+    res = requests.post(url, data=payload)
+    assert res.status_code == 400
+    assert res.json() == expected
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    Login with wrong password testing
+    """
+    url = domain % "sessions"
+    payload = [("email", email), ("password", password)]
+    expected = {"error": "Unauthorized"}
+    res = requests.post(url, data=payload)
+    assert res.status_code == 401
+    assert res.json() == expected
+    assert res.cookies == {}
+    assert res.cookies.get("session_id", None)
+
+
+def profile_unlogged() -> None:
+    """
+    Profile Unlogged Test
+    """
+    url = domain % "profile"
+    expected = {"error": "Forbidden"}
+    res = requests.get(url, cookies=dict())
+    assert res.status_code == 403
+    assert res.json() == expected
+
+
+def log_in(email: str, password: str) -> str:
+    """
+    Login Credentials Testing
+    """
+    url = domain % "sessions"
+    payload = [("email", email), ("password", password)]
+    expected = {"email": "%s" % email, "message": "logged in"}
+    res = requests.post(url, data=payload)
+    session_id = res.cookies.get("session_id", None)
+    assert res.status_code == 200
+    assert res.json() == expected
+    assert session_id is not None
+    assert session_id
+
+
+def profile_logged(session_id: str) -> None:
+    """
+    Logged Profile Testing
+    """
+    url = domain % "profile"
+    res = requests.get(url, cookies=dict(session_id=session_id))
+    assert res.status_code == 200
+    assert res.json().get("email") is not None
+
+
+def log_out(session_id: str) -> None:
+    """
+    Logout Call Testing
+    """
+    url = domain % "sessions"
+    res = requests.delete(url, cookies=dict(session_id=session_id))
+    print(res.status_code)
+    assert res.status_code == 200
+    assert res.json == {"message": "Bienvenue"}
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """
+    Updating Password Testing
+    """
+    url = domain % "reset_password"
+    payload = [
+        ("email", email),
+        ("reset_token", reset_token), ("new_password", new_password)
+    ]
+    expected = {"email": "%s" % email, "message": "Password updated"}
+    res = requests.put(url, data=payload)
+    assert res.status_code == 200
+    assert res.json() == expected
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
